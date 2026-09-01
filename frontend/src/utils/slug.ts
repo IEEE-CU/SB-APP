df4b067970d566e5bee2ab6c065c0074508124c0
ArrinPaul <yarrinpaul11@gmail.com>
2026-09-01 14:31:50 +0530

    fix(lint): fix unnecessary escape character errors in slug.ts

--- frontend/src/utils/slug.ts
+++ frontend/src/utils/slug.ts
@@ -11,8 +11,8 @@ export function slugify(text: string): string {
     .trim()
     .replace(/\s+/g, "-") // Replace spaces with -
     .replace(/&/g, "-and-") // Replace & with 'and'
-    .replace(/[^\w\-]+/g, "") // Remove all non-word chars
-    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/[^\w-]+/g, "") // Remove all non-word chars
+    .replace(/--+/g, "-") // Replace multiple - with single -
     .replace(/^-+/, "") // Trim - from start of text
     .replace(/-+$/, ""); // Trim - from end of text
 }
